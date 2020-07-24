7c91ec0a8d38c3580b0b896b5749fe3e6ab6ff51
Luis Cordoba <luiscordobahoyos@hotmail.com>
2020-07-24 15:27:35 -0500

    Initial Commit of infrastructure.Data Project, Update MVC for new UniversityDBContext

---
+++ CleanArch/CleanArch.Infra.Data/Context/UniversityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArch.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(DbContextOptions options) : base(options){
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
